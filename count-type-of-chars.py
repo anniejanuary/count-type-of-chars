9507ba017467d978305ec5d2c122b0107e3f7b02
anniejanuary <109144163+anniejanuary@users.noreply.github.com>
2022-07-31 16:26:38 +0200

    Create count-type-of-chars.py

---
+++ count-type-of-chars.py
@@ -0,0 +1,32 @@
+# Given a string S, write a program to count the occurrence of Lowercase characters, Uppercase characters, Special characters and Numeric values in the string.
+# Note: There are no white spaces in the string.
+# 
+# Example 1:
+# 
+# Input:
+# S = "#GeeKs01fOr@gEEks07"
+# Output:
+# 5
+# 8
+# 4
+# 2
+# Explanation: There are 5 uppercase characters,
+# 8 lowercase characters, 4 numeric characters
+# and 2 special characters.
+
+class Solution:
+    def count (self,s):
+        result = [0 for i in range(4)]
+        
+        for i in s:
+            if i >= 'A' and i <= 'Z':
+                result[0] += 1
+            elif i >= 'a' and i <= 'z':
+                result[1] += 1
+            elif i >= '0' and i <= '9':
+                result[2] += 1
+            else:
+                result[3] += 1
+        
+        return result
+        
